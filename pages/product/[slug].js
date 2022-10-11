96f1f090480c264d516f257b76123cd65d14f12a
iqbalhosen32 <iqbaldb32@gmail.com>
2022-10-11 11:15:45 +0600

    Banner buy now section done

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -24,74 +24,67 @@ const ProductDetais = ({ product, products, bannerProduct }) => {
 
     return (
         <div>
-            {
-                bannerProduct &&
-                <div>
-                    {
-                        bannerProduct.map((product, index) => (
-                            <div key={index} className='product-detail-container'>
+            {bannerProduct &&
+
+                <div className='product-detail-container'>
+                    <div>
+                        <div className="image-container">
+                            <picture>
+                                <img src={urlFor(bannerProduct.image && bannerProduct.image[index])} alt='' className='product-detail-image' />
+                            </picture>
+                        </div>
+                        <div className='small-images-container'>
+                            {
+                                bannerProduct.image?.map((item, i) => (
+                                    <picture key={i}>
+                                        <img src={urlFor(item)} className={i === index ? 'small-image selected-image' : 'small-image'} onMouseEnter={() => setIndex(i)} alt='' />
+                                    </picture>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <div className="product-detail">
+                        <div className="product-detail-desc">
+                            <h1>{bannerProduct.name}</h1>
+                            <div className="reviews">
                                 <div>
-                                    <div className="image-container">
-                                        <picture>
-                                            <img src={urlFor(product.image && product.image[index])} alt='' className='product-detail-image' />
-
-                                        </picture>
-                                    </div>
-                                    <div className='small-images-container'>
-                                        {
-                                            product?.image?.map((item, i) => (
-                                                <picture key={i}>
-                                                    <img src={urlFor(item)} className={i === index ? 'small-image selected-image' : 'small-image'} onMouseEnter={() => setIndex(i)} alt='' />
-                                                </picture>
-                                            ))
-                                        }
-                                    </div>
+                                    <AiFillStar />
+                                    <AiFillStar />
+                                    <AiFillStar />
+                                    <AiFillStar />
+                                    <AiOutlineStar />
                                 </div>
-                                <div className="product-detail">
-                                    <div className="product-detail-desc">
-                                        <h1>{product.name}</h1>
-                                        <div className="reviews">
-                                            <div>
-                                                <AiFillStar />
-                                                <AiFillStar />
-                                                <AiFillStar />
-                                                <AiFillStar />
-                                                <AiOutlineStar />
-                                            </div>
-                                            <p>
-                                                (20)
-                                            </p>
-                                        </div>
-                                        <h4>Details: </h4>
-                                        <p>{product.details}</p>
-                                        <div className='product-price'>
-
-                                            <p className='product-price-new price'>${product.newPrice}</p>
-                                            {
-                                                product.oldPrice && <p className='product-price-old price'>${product.oldPrice}</p>
-                                            }
-                                        </div>
-                                        <div className='quantity'>
-                                            <h3>Quantity:</h3>
-                                            <p className='quantity-desc'>
-                                                <span className='minus' onClick={decQty}><AiOutlineMinus /></span>
-                                                <span className='num'>{qty}</span>
-                                                <span className='plus' onClick={incQty}><AiOutlinePlus /></span>
-                                            </p>
-                                        </div>
-                                        <div className='buttons'>
-                                            <button type='button' onClick={() => onAdd(product, qty)} className='add-to-cart'>
-                                                Add To Cart
-                                            </button>
-                                            <button type='button' onClick={handleBuyNow} className='buy-now'>
-                                                Buy Now
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
+                                <p>
+                                    (20)
+                                </p>
                             </div>
-                        ))
-                    }
+                            <h4>Details: </h4>
+                            <p>{bannerProduct.details}</p>
+                            <div className='product-price'>
+
+                                <p className='product-price-new price'>${bannerProduct.newPrice}</p>
+                                {
+                                    bannerProduct.oldPrice && <p className='product-price-old price'>${bannerProduct.oldPrice}</p>
+                                }
+                            </div>
+                            <div className='quantity'>
+                                <h3>Quantity:</h3>
+                                <p className='quantity-desc'>
+                                    <span className='minus' onClick={decQty}><AiOutlineMinus /></span>
+                                    <span className='num'>{qty}</span>
+                                    <span className='plus' onClick={incQty}><AiOutlinePlus /></span>
+                                </p>
+                            </div>
+                            <div className='buttons'>
+                                <button type='button' onClick={() => onAdd(bannerProduct, qty)} className='add-to-cart'>
+                                    Add To Cart
+                                </button>
+                                <button type='button' onClick={handleBuyNow} className='buy-now'>
+                                    Buy Now
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             }
             {
@@ -204,7 +197,7 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async ({ params: { slug } }) => {
     const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
-    const queryBanner = `*[_type == "banner" && slug.current == '${slug}']`;
+    const queryBanner = `*[_type == "banner" && slug.current == '${slug}'][0]`;
     const productsQuery = '*[_type == "product"]'
     // const queryBanner = '*[_type == "banner"]'
 
